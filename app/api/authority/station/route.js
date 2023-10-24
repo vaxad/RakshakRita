3a2895eb404be9cc5d005185968135e72f96ffa9
vaxad <varadprabhu111@gmail.com>
2023-10-24 21:58:00 +0530

    dynamic usage issue

--- app/api/authority/station/route.js
+++ app/api/authority/station/route.js
@@ -8,51 +8,7 @@ import Authorities from "@/lib/db/models/Authorities";
 import { headers } from "next/headers";
 import jwt from "jsonwebtoken";
 
-// export async function POST(req) {
-//     try {
-//         // const client = await clientPromise;
-//         const db = await connect()
-//         //    console.log(db)
-//         let bodyObject = await req.json();
-        
-//         let myPost = await Stations.create({ "name": bodyObject.name, "latitudes": bodyObject.latitudes, "longitudes":bodyObject.longitudes, "qr":"","state":bodyObject.state, "taluka":bodyObject.taluka, "district":bodyObject.district, "village":bodyObject.village });
-//         console.log(myPost)
-//         const qrData = process.env.DOMAIN+"/feedback/"+myPost._id
-//         console.log(qrData)
-//         // Generate a QR code from the JSON string
-//         qrcode.toDataURL(qrData, async (err, url) => {
-//             if (err) {
-//                 console.error(err);
-//                 return NextResponse.json("not ok")
-
-//             } else {
-//                 // Upload the QR code to Cloudinary
-
-//                 const formData = new FormData();
-//                 formData.append('file', url);
-//                 formData.append('upload_preset', 'gcdc_test');
-//                 const response = await axios.post(
-//                     'https://api.cloudinary.com/v1_1/db670bhmc/image/upload',
-//                     formData
-//                 );
-
-//                 //.log(response.statusText);
-//                 if (response.statusText === "OK") {
-//                     // let myPost = await Stations.create({ "name": bodyObject.name, "loc": bodyObject.loc, "img": response.data.url });
-//                     myPost.qr = response.data.url
-//                     myPost.save()
-//                 } else {
-//                     return NextResponse.json("not ok")
-
-//                 }
-//             }
-//         });
-//         return NextResponse.json("ok")
-//     } catch (error) {
-//         console.log(error)
-//         return NextResponse.json(error)
-//     }
-// }
+export const dynamic = 'force-dynamic'
 
 export async function GET(req){
     try {
