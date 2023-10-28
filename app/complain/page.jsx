f97473f1c1c4eb8e77739161b0bc6d2fb19c73eb
vaxad <varadprabhu111@gmail.com>
2023-10-29 03:16:39 +0530

    removed qr

--- app/complain/page.jsx
+++ app/complain/page.jsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import QrReader from "react-qr-reader";
+// import QrReader from "react-qr-reader";
 import Navbar from '../components/Navbar';
 import Link from 'next/link';
 import QrScan from './components/qrScan';
